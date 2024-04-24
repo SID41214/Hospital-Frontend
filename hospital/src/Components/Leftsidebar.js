eeafd183f79dce01122e2e6de7b53b432ae2dd8b
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-04-24 16:52:57 +0530

    updated

---
+++ hospital/src/Components/Leftsidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Leftsidebar.css';
+const Leftsidebar = () => {
+  return (
+    <div>
+      <div className="container">
+dghssssdfhfsdfhs
+      </div>
+    </div>
+  )
+}
+
+export default Leftsidebar
--- hospital/src/Pages/Home.js
+++ hospital/src/Pages/Home.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import './Home.css';
-
+import React from "react";
+import "./Home.css";
+// import Leftsidebar from "../Components/Leftsidebar";
 const Home = () => {
   return (
     <div>
-      <div className='container'> 
-            <div className='topbar'>
-                <div>
-                    Hospital Management Systems
-                </div>
-                <div className="dis">
-                    <ul>
-                        <li>Admin</li>
-                        <li>Doctor</li>
-                        <li>Users</li>
-                    </ul>
-
-                </div>
-                <div className="other">
-                    <ul>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
-                       
-                    </ul>
-
-                </div>
-            </div>
+      <div className="container">
+        <div className="topbar">
+          <div>Hospital Management Systems</div>
+          <div className="dis">
+            <ul>
+              <li>Admin</li>
+              <li>Doctor</li>
+              <li>Users</li>
+            </ul>
+          </div>
+          <div className="other">
+            <ul>
+              <li>About Us</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-
-
+      {/* <div className="left">
+        <div  className="left-sidebar">
+              <Leftsidebar/>
+        </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
